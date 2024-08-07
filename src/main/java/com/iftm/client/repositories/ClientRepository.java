55769ce9ce57841d89016279bdb7fafd307c7755
Vini-IFTM <vinicius.raphael@estudante.iftm.edu.br>
2024-08-06 21:32:59 -0300

    Carlos - Teste procurar cliente entre período específico

--- src/main/java/com/iftm/client/repositories/ClientRepository.java
+++ src/main/java/com/iftm/client/repositories/ClientRepository.java
@@ -1,5 +1,8 @@
 package com.iftm.client.repositories;
 
+import java.time.Instant;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,7 @@ import com.iftm.client.entities.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    List<Client> findClientByBirthDateBetween(Instant dataInicio, Instant dataTermino);
 
 }

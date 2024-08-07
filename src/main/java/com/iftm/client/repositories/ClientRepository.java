7c9421212295f7e1917ffee23809f5eb2738073b
Lucas <108193174+lucas-azv@users.noreply.github.com>
2024-08-06 21:49:53 -0300

    Add files via upload

--- src/main/java/com/iftm/client/repositories/ClientRepository.java
+++ src/main/java/com/iftm/client/repositories/ClientRepository.java
@@ -1,16 +1,23 @@
 package com.iftm.client.repositories;
 
-import java.time.Instant;
-import java.util.List;
-
+import com.iftm.client.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.iftm.client.entities.Client;
+import java.time.Instant;
+import java.util.List;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
-    List<Client> findClientByBirthDateBetween(Instant dataInicio, Instant dataTermino);
+    @Query(value = "SELECT * FROM tb_client WHERE income > :income", nativeQuery = true)
+    List<Client> findByIncomeGreaterThan(Double income);
+
+    @Query(value = "SELECT * FROM tb_client WHERE income < :income", nativeQuery = true)
+    List<Client> findByIncomeLessThan(Double income);
+
+    @Query(value = "SELECT * FROM tb_client WHERE income BETWEEN :minIncome AND :maxIncome", nativeQuery = true)
+    List<Client> findByIncomeBetween(Double minIncome, Double maxIncome);
 
 }

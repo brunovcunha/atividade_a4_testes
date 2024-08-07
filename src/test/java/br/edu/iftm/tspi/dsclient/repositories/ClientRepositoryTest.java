bf66fe6938a6a83f8cd403547e0098ce4ae10f15
João Vitorino V. Neto <133411181+JoaoVitorinoVN@users.noreply.github.com>
2024-08-07 00:00:51 -0300

    Aplicação dos testes - João
    > Achar e não achar por ignorecase
    > Achar uma lista usando "containing"  + ignorecase
    > Inserção de um cliente novo

--- src/test/java/br/edu/iftm/tspi/dsclient/repositories/ClientRepositoryTest.java
+++ src/test/java/br/edu/iftm/tspi/dsclient/repositories/ClientRepositoryTest.java
@@ -2,7 +2,7 @@ package br.edu.iftm.tspi.dsclient.repositories;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.Instant;
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import br.edu.iftm.tspi.dsclient.entities.Client;
-import jakarta.persistence.EntityNotFoundException;
 
 @DataJpaTest
 public class ClientRepositoryTest {
@@ -43,16 +42,28 @@ public class ClientRepositoryTest {
         assertEquals(0, clients.size());
     }
 
+    // Bruno Vieira
     @Test
     @DisplayName("Testar a exclusao de um cliente por id")
     public void testDeleteClienteExistente() {
 
-        Long clienteId = (long)1;
+        Long clienteId = (long) 1;
 
         repository.deleteById(clienteId);
 
         Optional<Client> clienteDeletado = repository.findById(clienteId);
         assertFalse(clienteDeletado.isPresent());
+    }
+
+    // Bruno Vieira
+    @Test
+    @DisplayName("Testa o findAll para retornar todos os clientes que estão na base de dados")
+    public void testFindAll() {
+
+        List<Client> clientes = repository.findAll();
+
+        assertNotNull(clientes);
+        assertEquals(12, clientes.size());
     }
 
     @Test
@@ -138,6 +149,88 @@ public class ClientRepositoryTest {
     public void testFindByIncomeBetweenNonExistingRange() {
         List<Client> clients = repository.findByIncomeBetween(8000.0, 10000.0);
         assertEquals(1, clients.size());
+    }
+
+    // Lucas Azevedo
+    @Test
+    public void testFindByIncomeGreaterThan() {
+        Client client1 = new Client(1L, "Lázaro Ramos", "10619244881", 2500.0, Instant.parse("1996-12-23T07:00:00Z"), 2);
+        Client client2 = new Client(2L, "Gilberto Gil", "10419344882", 8000.0, Instant.parse("1949-05-05T07:00:00Z"), 4);
+
+        repository.saveAll(List.of(client1, client2));
+
+        List<Client> clients = repository.findByIncomeGreaterThan(3000.0);
+        assertEquals(1, clients.size());
+        assertEquals("Gilberto Gil", clients.get(0).getName());
+    }
+
+    // Lucas Azevedo  
+    @Test
+    public void testFindByIncomeLessThan() {
+        Client client1 = new Client(1L, "Lázaro Ramos", "10619244881", 2500.0, Instant.parse("1996-12-23T07:00:00Z"), 2);
+        Client client2 = new Client(2L, "Gilberto Gil", "10419344882", 8000.0, Instant.parse("1949-05-05T07:00:00Z"), 4);
+
+        repository.saveAll(List.of(client1, client2));
+
+        List<Client> clients = repository.findByIncomeLessThan(5000.0);
+        assertEquals(1, clients.size());
+        assertEquals("Lázaro Ramos", clients.get(0).getName());
+    }
+
+    // Lucas Azevedo
+    @Test
+    public void testFindByIncomeBetween() {
+        Client client1 = new Client(1L, "Lázaro Ramos", "10619244881", 2500.0, Instant.parse("1996-12-23T07:00:00Z"), 2);
+        Client client2 = new Client(2L, "Gilberto Gil", "10419344882", 8000.0, Instant.parse("1949-05-05T07:00:00Z"), 4);
+        Client client3 = new Client(3L, "Djamila Ribeiro", "10619244884", 4500.0, Instant.parse("1975-11-10T07:00:00Z"), 1);
+
+        repository.saveAll(List.of(client1, client2, client3));
+
+        List<Client> clients = repository.findByIncomeBetween(2000.0, 5000.0);
+        assertEquals(2, clients.size());
+        assertEquals("Lázaro Ramos", clients.get(0).getName());
+        assertEquals("Djamila Ribeiro", clients.get(1).getName());
+    }
+    
+    // João
+    @Test
+    @DisplayName("Verifica nome existente ignorando a case da consulta.")
+    public void testFindExistingNameIgnoringCase() {
+        List<Client> clients = repository.findByNameContainingIgnoreCase("conceição");
+        assertEquals(1, clients.size());
+        assertEquals("Conceição Evaristo", clients.get(0).getName());
+    }
+
+    // João
+    @Test
+    @DisplayName("Verifica se nomes inexistentes são encontrados com a case ignorada")
+    public void testFindInexistingNameIgnoringCase() {
+        List<Client> clients = repository.findByNameContainingIgnoreCase("joao vitorino");
+        assertEquals(0, clients.size());
+    }
+
+    // João
+    @Test
+    @DisplayName("Verifica a consulta de uma lista com IgnoreCase + Contains")
+    public void testFindListOfExistingNamesIgnoringCase() {
+        List<Client> clients = repository.findByNameContainingIgnoreCase("ra");
+        assertEquals(3, clients.size());
+        assertEquals("Lázaro Ramos", clients.get(0).getName());
+        assertEquals("Jose Saramago", clients.get(1).getName());
+        assertEquals("Yuval Noah Harari", clients.get(2).getName());
+    }
+
+    // João
+    @Test
+    @DisplayName("Verifica a criação de um usuário válido e atribuição de ID automática")
+    public void testCreateNewValidClient() {
+        Client validClient = new Client(null, "João Vitorino", "12345678900", 1500.0,
+                Instant.parse("2005-04-19T00:00:00Z"), 0);
+        Client savedClient = repository.save(validClient);
+        Optional<Client> retrievedClient = repository.findById(13L);
+
+        assertTrue(retrievedClient.isPresent(), "O cliente salvo deve estar presente.");
+        assertEquals(savedClient, retrievedClient.get());
     }
 
 }

135ed93d0fe8ac2abdd8f0b3e2703599d1747bc6
ViniRaphael <vinicius.raphael1311@gmail.com>
2024-08-13 19:37:39 -0300

    test

--- src/test/java/br/edu/iftm/tspi/dsclient/repositories/ClientRepositoryTest.java
+++ src/test/java/br/edu/iftm/tspi/dsclient/repositories/ClientRepositoryTest.java
@@ -23,6 +23,7 @@ public class ClientRepositoryTest {
     @Autowired
     private ClientRepository repository;
 
+    // test
     // Bruno Vieira
     @Test
     @DisplayName("Esse teste verifica se o metodo findByName retorna um cliente com um nome existente na base de dados")
